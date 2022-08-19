b4520ab58811a8efa850708991e570d72b92f042
Enrique Mejías Álvarez <enrique.mejias@envato.com>
2022-08-18 22:53:19 -0500

    Updates to the save to the database feature

--- src/database.rs
+++ src/database.rs
@@ -30,8 +30,9 @@ impl Graph {
 
         let builder = client.from(GRAPH_TABLE_NAME);
         let operation = match &self.id {
-            Some(id) => builder.eq("id", id.as_str()).update(&self.partial_to_string().unwrap()),
-            None => builder.insert(&self.to_string().unwrap())
+            Some(id) => builder.eq("id", id.as_str())
+                               .update(&self.partial_to_serde_string()),
+            None => builder.insert(&self.to_serde_string())
         };
         let resp = operation.execute().await?;
         let _body = resp.text().await?;
@@ -39,23 +40,23 @@ impl Graph {
         Ok(())
     }
 
-    fn to_string(&self) -> serde_json::Result<String> {
-        serde_json::to_string(self)
+    fn to_serde_string(&self) -> String {
+        serde_json::to_string(self).unwrap()
     }
 
-    fn partial_to_string(&self) -> serde_json::Result<String> {
+    fn partial_to_serde_string(&self) -> String {
         let partial = serde_json::json!({
             "code": self.code.clone(),
             "name": self.name.clone(),
         });
-        serde_json::to_string(&partial)
+        serde_json::to_string(&partial).unwrap()
     }
 }
 
 pub async fn insert_on_db(code: &str) -> Result<(), Box<dyn std::error::Error>> {
     let graph = Graph {
         id: Some("25e007b9-f12b-4396-899f-5d1cd454ab98".to_string()),
-        name: "After adding id (Updated)".to_string(),
+        name: "After adding id (Updated v2)".to_string(),
         user_id: "7febcbe7-a9d4-48b4-99c5-8c1f290ae934".to_string(),
         code: code.to_string()
     };
