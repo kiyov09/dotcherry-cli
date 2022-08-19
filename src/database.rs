1af8cc61bdcbdef80d03a1a502f85c7992203b54
Enrique Mejías Álvarez <enrique.mejias@envato.com>
2022-08-18 22:07:45 -0500

    Refactor the Graph struct

--- src/database.rs
+++ src/database.rs
@@ -13,6 +13,8 @@ fn get_postgrest_client() -> postgrest::Postgrest {
     Postgrest::new(supabase_api_url).insert_header("apikey", supabase_api_key)
 }
 
+static GRAPH_TABLE_NAME: &str = "graphs";
+
 #[derive(Debug, Serialize)]
 struct Gragh {
     name: String,
@@ -20,26 +22,38 @@ struct Gragh {
     code: String
 }
 
-pub async fn insert_on_db(code: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let client = get_postgrest_client();
+impl Gragh {
+    async fn save(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let client = get_postgrest_client();
 
+        let resp = client
+            .from(GRAPH_TABLE_NAME)
+            .insert(
+                self.to_string().unwrap()
+            )
+            .execute()
+            .await?;
+
+        let body = resp.text().await?;
+
+        println!("{:}", body);
+
+        Ok(())
+    }
+
+    fn to_string(&self) -> serde_json::Result<String> {
+        serde_json::to_string(self)
+    }
+}
+
+pub async fn insert_on_db(code: &str) -> Result<(), Box<dyn std::error::Error>> {
     let graph = Gragh {
-        name: "After removing dotenv".to_string(),
+        name: "Inserted from Graph method".to_string(),
         user_id: "7febcbe7-a9d4-48b4-99c5-8c1f290ae934".to_string(),
         code: code.to_string()
     };
 
-    let resp = client
-        .from("graphs")
-        .insert(
-            serde_json::to_string(&graph).unwrap()
-        )
-        .execute()
-        .await?;
-
-    let body = resp.text().await?;
-
-    println!("{:}", body);
+    graph.save().await?;
 
     Ok(())
 }
